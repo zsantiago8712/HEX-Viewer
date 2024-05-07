d89e93bccbdc1939a9a1d7c98144f54629c0806c
Santiago Zamora <zsantiago81@icloud.com>
2024-05-06 19:29:53 -0600

    Update .gitignore, app.h, premake5.lua, main.c, dynamicAllocator.c, README.md, premake5.lua, xmake.lua

--- Hex/include/Core/app.h
+++ Hex/include/Core/app.h
@@ -4,16 +4,16 @@
 
 #define EmptyApp                                                        \
     {                                                                   \
-        true, EmptyWindowBuffer, EmptyWindowBuffer, EmptyWindowBuffer, \
-            EmptyWindowTreeFile, EmptyCursor                                         \
+        EmptyWindowBuffer, EmptyWindowBuffer, EmptyWindowBuffer, \
+            EmptyWindowTreeFile, true,  EmptyCursor                                         \
     }
 
 typedef struct App {
-    bool is_running;
     WindowBuffer window_hex;
     WindowBuffer window_ascii;
     WindowBuffer window_padding;
     WindowTreeFile window_tree_file;
+    bool is_running;
     Cursor cursor;
 } App;
 
